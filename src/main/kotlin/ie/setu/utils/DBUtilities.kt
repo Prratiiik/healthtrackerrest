3a015e527a9f966412d5069a263fb235ed4c3ba2
Prratiiik <144805450+Prratiiik@users.noreply.github.com>
2024-08-31 21:23:58 +0100

    added blood pressure activity data layer

--- src/main/kotlin/ie/setu/utils/DBUtilities.kt
+++ src/main/kotlin/ie/setu/utils/DBUtilities.kt
@@ -2,7 +2,9 @@ package ie.setu.utils
 
 import ie.setu.domain.Activity
 import ie.setu.domain.User
+import ie.setu.domain.BloodPressure
 import ie.setu.domain.db.Activities
+import ie.setu.domain.db.BloodPressures
 import ie.setu.domain.db.Users
 import org.jetbrains.exposed.sql.ResultRow
 
@@ -17,4 +19,13 @@ fun mapToActivities(it: ResultRow) = Activity(
     id = it[Activities.id],
     activityName = it[Activities.activityName],
     calories = it[Activities.calories],
+    userId = it[Activities.userId]
+)
+
+fun mapToBloodPressure(it:ResultRow): BloodPressure = BloodPressure(
+    id = it[BloodPressures.id],
+    systolic = it[BloodPressures.systolic],
+    diastolic = it[BloodPressures.diastolic],
+    pulse = it[BloodPressures.pulse],
+    userId = it[BloodPressures.userId]
 )
