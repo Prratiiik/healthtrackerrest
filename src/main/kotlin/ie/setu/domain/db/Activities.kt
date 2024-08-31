48be02bb328fb38ec9a26689c65ac453584f2be3
Prratiiik <144805450+Prratiiik@users.noreply.github.com>
2024-08-31 22:04:00 +0100

    added data layer for temperature plus added created at timestamp in all

--- src/main/kotlin/ie/setu/domain/db/Activities.kt
+++ src/main/kotlin/ie/setu/domain/db/Activities.kt
@@ -7,4 +7,6 @@ object Activities:Table("activities") {
     val id = integer("id").autoIncrement().primaryKey()
     val calories = varchar("calories",255)
     val activityName = varchar("activityName",255)
+    val userId = integer("userId").references(Users.id, onDelete = ReferenceOption.CASCADE, onUpdate = ReferenceOption.CASCADE)
+    val createdAt = datetime("createdAt")
 }
