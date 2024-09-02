876ab4ae6452e3dd6022eb8043d4afa82f5e3922
Prratiiik <144805450+Prratiiik@users.noreply.github.com>
2024-09-02 23:05:23 +0100

    Running db

--- src/main/kotlin/ie/setu/config/DBConfig.kt
+++ src/main/kotlin/ie/setu/config/DBConfig.kt
@@ -5,6 +5,10 @@ import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.name
 import org.jetbrains.exposed.sql.transactions.transaction
+import java.sql.DriverManager
+
+
+
 
 class DbConfig{
 
@@ -16,11 +20,18 @@ class DbConfig{
         logger.info{"Starting DB Connection..."}
 
         val dbConfig = Database.connect(
-            "jdbc:postgresql://lucky.db.elephantsql.com:5432/eflinqga?sslmode=require",
+            /*"jdbc:postgresql://lucky.db.elephantsql.com:5432/eflinqga?sslmode=require",*/
+           "jdbc:postgresql://manny.db.elephantsql.com:5432/olvgnvwq",
             driver = "org.postgresql.Driver",
-            user = "eflinqga",
-            password = "0We1w7m9NbtoJssZQ89waH8rr4tg7KQR"
+            user = "olvgnvwq",
+            password = "7XJN4W1TDfuT9Dx0mA0TBEOcyYU6CSWF"
         )
+       /* val url = "jdbc:postgresql://manny.db.elephantsql.com:5432/olvgnvwq"
+        val user = "olvgnvwq"
+        val password = "7XJN4W1TDfuT9Dx0mA0TBEOcyYU6CSWF"*/
+
+        /*val conn: Connection = DriverManager.getConnection(url, user, password)*/
+
 
         transaction{
             SchemaUtils.createMissingTablesAndColumns(Users,Activities, BloodPressures,Runings,Temperatures)
--- src/test/kotlin/ie/setu/helpers/Fixtures.kt
+++ src/test/kotlin/ie/setu/helpers/Fixtures.kt
@@ -4,21 +4,22 @@ import ie.setu.domain.Activity
 import ie.setu.domain.User
 import ie.setu.domain.db.Activities.calories
 import ie.setu.domain.db.Activities.id
+import org.joda.time.DateTime
 
 val nonExistingEmail = "112233445566778testUser@xxxxx.xx"
 val validName = "Test User 1"
 val validEmail = "testuser1@test.com"
 
 val users = arrayListOf<User>(
-    User(name = "Roger", email = "roger@setu.com", id = 1, password = "password"),
-    User(name = "Alice", email = "alice@setu.ie", id = 2, password = "password"),
-    User(name = "John", email = "john@gmail.com", id = 3, password = "password"),
-    User(name = "Steve", email = "steve@apple.com", id = 4, password = "password")
+    User(name = "Roger", email = "roger@setu.com", id = 1, password = ""),
+    User(name = "Alice", email = "alice@setu.ie", id = 2, password = ""),
+    User(name = "John", email = "john@gmail.com", id = 3, password = ""),
+    User(name = "Steve", email = "steve@apple.com", id = 4, password = "")
 )
 
 val activities = arrayListOf<Activity>(
-    Activity(calories = "10", activityName = "Task1", id = 1, userId = 1),
-    Activity(calories = "20", activityName = "Task2", id = 2, userId = 1),
-    Activity(calories = "30", activityName = "Task3", id = 3, userId = 1)
+    Activity(calories = "10", activityName = "Task1", id = 1, userId = 1, createdAt = DateTime.now()),
+    Activity(calories = "20", activityName = "Task2", id = 2, userId = 2, createdAt = DateTime.now()),
+    Activity(calories = "30", activityName = "Task3", id = 3, userId = 3, createdAt = DateTime.now())
 
 )
